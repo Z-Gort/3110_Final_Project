666bf8b3e0563c8037c280e6dcc3e9beb7d2ee98
Andrej Dovciak <ajd286@cornell.edu>
2024-03-25 11:12:41 -0400

    Add comment to game.mli

--- poker/lib/game.mli
+++ poker/lib/game.mli
@@ -9,3 +9,6 @@ type t = {
 
 val newgame : t
 (** [newgame] is a brand new game with two cards dealt to every player*)
+
+(* Ideas for the future: create a player compilation unit that so that players
+   are represented as type Player.t rather than (int * Hand.t) list. *)
