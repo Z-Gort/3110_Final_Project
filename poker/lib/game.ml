ba23d4a01217d99ec45a3eb8f7f5261547cd3b88
Andrej Dovciak <ajd286@cornell.edu>
2024-03-25 01:15:31 -0400

    Make additions to game
    - functions for dealing the first two cards out to players

--- poker/lib/game.ml
+++ poker/lib/game.ml
@@ -1,9 +1,9 @@
 type t = {
-  players : int list;
+  players : (int * Hand.t) list;
   deck : Card.t list;
-  hands : Hand.t list;
 }
 
+(* Represents a brand new deck of cards *)
 let newdeck : Card.t list =
   [
     { suit = Clubs; rank = Two };
@@ -60,7 +60,73 @@ let newdeck : Card.t list =
     { suit = Diamonds; rank = Ace };
   ]
 
-(* let deal_round *)
+let () = Random.self_init ()
 
-let newgame () = { players = [ 1; 2; 3; 4; 5; 6 ]; deck = newdeck; hands = [] }
-(* unimplemented *)
+(** [rem_card n d i] is the deck [d] with the card [n] cards away from index [i]
+    removed. *)
+let rec rem_card n d i =
+  match d with
+  | h :: t -> if i = n then t else h :: rem_card n t (i + 1)
+  | [] -> []
+
+(** [new_cards d] pulls 6 cards randomly out of the deck [d] (to simulate
+    drawing from the top of a shuffled deck) and returns those cards as the
+    first 6 entries in a 7-tuple and the 7th entry as the [d] after those cards
+    are removed *)
+let new_cards d =
+  let decksize = List.length d in
+  let i1 = Random.int decksize in
+  let i2 = Random.int (decksize - 1) in
+  let i3 = Random.int (decksize - 2) in
+  let i4 = Random.int (decksize - 3) in
+  let i5 = Random.int (decksize - 4) in
+  let i6 = Random.int (decksize - 5) in
+  let c1 = List.nth d i1 in
+  let c2 = List.nth d i2 in
+  let c3 = List.nth d i3 in
+  let c4 = List.nth d i4 in
+  let c5 = List.nth d i5 in
+  let c6 = List.nth d i6 in
+  let nextd1 = rem_card i1 d 0 in
+  let nextd2 = rem_card i2 nextd1 0 in
+  let nextd3 = rem_card i3 nextd2 0 in
+  let nextd4 = rem_card i4 nextd3 0 in
+  let nextd5 = rem_card i5 nextd4 0 in
+  let nextdeck = rem_card i6 nextd5 0 in
+  (c1, c2, c3, c4, c5, c6, nextdeck)
+
+(** [deal_cards g] pulls 6 cards out of [g.deck] and adds them to the hands of
+    the players represented by [g.players] *)
+let deal_cards g =
+  match g.players with
+  | [ (p1, h1); (p2, h2); (p3, h3); (p4, h4); (p5, h5); (p6, h6) ] -> begin
+      match new_cards g.deck with
+      | c1, c2, c3, c4, c5, c6, d ->
+          {
+            players =
+              [
+                (p1, Hand.add c1 h1);
+                (p2, Hand.add c2 h2);
+                (p3, Hand.add c3 h3);
+                (p4, Hand.add c4 h4);
+                (p5, Hand.add c5 h5);
+                (p6, Hand.add c6 h6);
+              ];
+            deck = d;
+          }
+    end
+  | _ -> g
+
+let newgame =
+  {
+    players =
+      [
+        (1, Hand.empty);
+        (2, Hand.empty);
+        (3, Hand.empty);
+        (4, Hand.empty);
+        (5, Hand.empty);
+        (6, Hand.empty);
+      ];
+    deck = newdeck;
+  }
--- poker/lib/game.mli
+++ poker/lib/game.mli
@@ -1,16 +1,15 @@
 type t = {
-  players : int list;
+  players : (int * Hand.t) list;
   deck : Card.t list;
-  hands : Hand.t list;
 }
 (** Represents a game with (6) players with a list of integers corresponding to
     the number of the player in the order that they will be dealt cards (game
     begins with player 1 being dealt a card first), a deck, and the hands of the
     players (order of hands corresponds with order of players) *)
 
-val newgame : unit -> t
-(** [newgame ()] is a brand new game only *)
+val newgame : t
+(** [newgame] is a brand new game with no cards yet dealt *)
 
 val deal_cards : t -> t
-(** [deal_cards g] is the game g but with a cards dealt off the top of the deck
-    until *)
+(** [deal_cards g] is the game g but with cards dealt off the top of the deck
+    until each player has recieved a new card*)
