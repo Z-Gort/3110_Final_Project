8b1a503723a32d8e9dd744bd7631e27aa9e374e3
Andrej Dovciak <ajd286@cornell.edu>
2024-04-26 16:06:03 -0400

    Modify start_game slightly

--- poker/bin/main.ml
+++ poker/bin/main.ml
@@ -1,17 +1,17 @@
 open Poker
 
+let bet () = ()
+
 let start_game () =
   let _ = print_endline "You are playing Poker! (Texas hold 'em rules)" in
   let start = Game.newgame in
   let _ = print_endline "These are your cards to start the game:" in
-  match start.players with
-  | p1 :: _ -> print_endline (Hand.string_of_hand p1.hand)
-  | _ -> ()
-;;
+  let _ =
+    match start.players with
+    | p1 :: _ -> print_endline (Hand.string_of_hand p1.hand)
+    | _ -> ()
+  in
+  bet ()
+(* let flop () = *)
 
-let flop () = 
-  
 (* bet () *)
-
-let _ = start_game ()
-let _ = continue_game ()
